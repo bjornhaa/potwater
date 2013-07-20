3862309265468cdb765bd442ccf4abca7f13f291
Bjørn Haakenstad <bjorn@haakenstad.com>
2013-07-20 21:36:43 +0200

    fixed debug

--- src/main/java/com/haakenstad/potwater/scheduler/PotwaterScheduler.java
+++ src/main/java/com/haakenstad/potwater/scheduler/PotwaterScheduler.java
@@ -1,5 +1,6 @@
 package com.haakenstad.potwater.scheduler;
 
+import org.apache.commons.lang3.StringUtils;
 import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
 import org.slf4j.Logger;
@@ -25,18 +26,21 @@ public class PotwaterScheduler {
     private static final Integer VALVE_3 = 5;
     private static final Integer VALVE_4 = 4;
 
-    private static final Integer DURATION_VALVE_1 = 15;
-    private static final Integer DURATION_VALVE_2 = 15;
-    private static final Integer DURATION_VALVE_3 = 15;
+    private static final Integer DURATION_VALVE_1 = 10;
+    private static final Integer DURATION_VALVE_2 = 10;
+    private static final Integer DURATION_VALVE_3 = 10;
     private static final Integer DURATION_VALVE_4 = 25;
 
     private static final Integer PUMP = 2;
 
     public static void main(String[] args) throws Exception {
         System.out.println("*********** Starting PotWater ****************");
+        System.out.println("Args:" + StringUtils.join(args));
         boolean debug = false;
         if (args.length > 0) {
-            debug = true;
+            if ("debug".equalsIgnoreCase(args[0])) {
+                debug = true;
+            }
         }
 
 
@@ -47,12 +51,12 @@ public class PotwaterScheduler {
 
         JobDetail job = newJob(PotWaterJob.class)
                 .withIdentity("wetjob")
-                .usingJobData("debug",debug)
-                .usingJobData("pump",PUMP)
-                .usingJobData(VALVE_1.toString(),DURATION_VALVE_1)
-                .usingJobData(VALVE_2.toString(),DURATION_VALVE_2)
-                .usingJobData(VALVE_3.toString(),DURATION_VALVE_3)
-                .usingJobData(VALVE_4.toString(),DURATION_VALVE_4)
+                .usingJobData("debug", debug)
+                .usingJobData("pump", PUMP)
+                .usingJobData(VALVE_1.toString(), DURATION_VALVE_1)
+                .usingJobData(VALVE_2.toString(), DURATION_VALVE_2)
+                .usingJobData(VALVE_3.toString(), DURATION_VALVE_3)
+                .usingJobData(VALVE_4.toString(), DURATION_VALVE_4)
                 .build();
 
         CronTrigger trigger = newTrigger()
